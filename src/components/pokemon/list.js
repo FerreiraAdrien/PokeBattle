2bc64ea73aa153e5fe78ed70153f39e0b579dce1
ThomasBerranger <tberranger@hotmail.fr>
2020-03-16 19:31:34 +0100

    list card

--- src/components/pokemon/list.js
+++ src/components/pokemon/list.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Wrapper from '../wrapper'
 
 const List = () => {
   return (
     <div>
-      <Wrapper>List of pokemon</Wrapper>
+      <Wrapper>
+        List of pokemon :
+        <br />
+        <br />
+        <br />
+        <br />
+        <CardStyled>information sur pokemon</CardStyled>
+        <br />
+        <br />
+        <br />
+      </Wrapper>
     </div>
   )
 }
+
+const CardStyled = styled.div`
+  background-color: burlywood;
+  width: 90%;
+  padding: 2%;
+  border-radius: 5px;
+  margin-left: auto;
+  margin-right: auto;
+`
 
 export default List
