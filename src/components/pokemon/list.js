ebfbb6013d09b90edf94891f5faa8802bb19a957
ThomasBerranger <tberranger@hotmail.fr>
2020-03-17 15:06:39 +0100

    card example

--- src/components/pokemon/list.js
+++ src/components/pokemon/list.js
@@ -12,7 +12,20 @@ const List = () => {
         <br />
         <br />
         <br />
-        <CardStyled>information sur pokemon</CardStyled>
+        <CardStyled>
+          <CardHeaderStyled>
+            <ImageStyled
+              src={
+                'https://www.pokepedia.fr/images/thumb/8/8d/Ectoplasma-RFVF.png/375px-Ectoplasma-RFVF.png'
+              }
+            ></ImageStyled>
+          </CardHeaderStyled>
+          <CardContentStyled>
+            <PokemonNameStyled>Ectoplasma</PokemonNameStyled>
+            <PokemonTypeStyled>Spectre</PokemonTypeStyled>
+            <PokemonTypeStyled>Poison</PokemonTypeStyled>
+          </CardContentStyled>
+        </CardStyled>
         <br />
         <br />
         <br />
@@ -22,12 +35,37 @@ const List = () => {
 }
 
 const CardStyled = styled.div`
-  background-color: burlywood;
   width: 90%;
   padding: 2%;
   border-radius: 5px;
   margin-left: auto;
   margin-right: auto;
+  box-shadow: 3px 3px 10px 5px grey;
+`
+
+const CardHeaderStyled = styled.div`
+  text-align: center;
+`
+
+const CardContentStyled = styled.div`
+  text-align: center;
+`
+
+const ImageStyled = styled.img`
+  height: 100px;
+  width: auto;
+`
+const PokemonNameStyled = styled.p`
+  font-size: 110%;
+`
+
+const PokemonTypeStyled = styled.span`
+  font-size: 90%;
+  margin: 2%;
+  padding: 3px 20px 6px 20px;
+  color: white;
+  background-color: royalblue;
+  border-radius: 20px;
 `
 
 export default List
