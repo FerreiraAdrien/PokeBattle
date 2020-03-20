b2854fc17ceedcc9aa99319934f65f1a9e8501c3
FerreiraAdrien <adrien.ferreira@ynov.com>
2020-03-20 11:27:25 +0100

    add i18n

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Routes from './config/routes/routes'
 
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import './config/i18n/translations'
 
 function App() {
   return (
