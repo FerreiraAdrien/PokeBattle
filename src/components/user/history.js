2afde4711766c469cbd9a54888db942f92a88f37
FerreiraAdrien <adrien.ferreira@ynov.com>
2020-03-20 16:56:50 +0100

    style battle history page

--- src/components/user/history.js
+++ src/components/user/history.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import i18n from 'i18n'
+import { withTranslation } from 'react-i18next'
 
 import Wrapper from '../wrapper'
 
-const History = () => {
-  return <Wrapper>My battle history</Wrapper>
+const History = ({ t }) => {
+  return (
+    <Wrapper>
+      <TitlePage>{t('battlehistory')}</TitlePage>
+    </Wrapper>
+  )
 }
 
-export default History
+const TitlePage = styled.h2`
+  text-align: center;
+  text-transform: uppercase;
+`
+
+export default withTranslation()(History)
