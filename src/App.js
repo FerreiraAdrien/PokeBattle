aca39ee347c5d617378835fe412bda850b2f7839
MickaelValentin <mickael.valentin@ynov.com>
2020-03-27 16:04:31 +0100

    push notifications available

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
+import { messaging } from './config/initFirebaseMessaging'
 
 import Routes from './config/routes/routes'
 
 function App() {
+  useEffect(() => {
+    messaging
+      .requestPermission()
+      .then(async function() {
+        const token = await messaging.getToken()
+        console.log('TCL: App -> token', token)
+      })
+      .catch(function(err) {
+        console.log('Unable to get permission to notify.', err)
+      })
+    navigator.serviceWorker.addEventListener('message', message =>
+      console.log(message)
+    )
+  })
   return (
     <div className='App'>
       <Routes></Routes>
