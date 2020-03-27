0146695a3a693821e9537199c9875d3c6314f0be
FerreiraAdrien <adrien.ferreira@ynov.com>
2020-03-27 11:21:28 +0100

    add function to put new pokemon in team in action redux

--- src/redux/actions/teamPokemon/index.js
+++ src/redux/actions/teamPokemon/index.js
@@ -1,12 +1,23 @@
-export const ADD_POKEMON = 'ADD_POKEMON'
-export const DELETE_POKEMON = 'DELETE_POKEMON'
+import axios from 'axios'
 
-export const AddPokemons = pokemon => ({
-  type: ADD_POKEMON,
-  payload: {
-    pokemon
-  }
-})
+export const DELETE_POKEMON = 'DELETE_POKEMON'
+export const ADD_POKEMON = 'ADD_POKEMON'
+
+export const AddPokemon = () => dispatch => {
+  let username = localStorage.getItem('username')
+  let poke = '6'
+
+  axios
+    .put('http://51.158.70.36:8080/api/dresseurs/' + username, {
+      poke_id: poke
+    })
+    .then(function(res) {
+      console.log(res.team)
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+}
 
 export const DeletePokemons = pokemon => ({
   type: DELETE_POKEMON,
